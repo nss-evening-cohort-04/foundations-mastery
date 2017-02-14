5c2e6ef3cc40973c09f906b9ae2dfaa3689ee6f7
sward42 <sarahward42@gmail.com>
2017-02-13 19:40:50 -0600

    add CustomString class

---
+++ FoundationsMastery/CustomString.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationsMastery
+{
+    public class CustomString : Interfaces.ICustomString
+    {
+        public IEnumerable<char> Contents { get; set; }
+
+        public int Length { get; }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Concat(IEnumerable<char> rhs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Interleave(IEnumerable<char> rhs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Print()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
