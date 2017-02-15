ed797f0569829ed45b5aedc1690f4ff58506c910
Debra Gordon <debra.m.gordon@gmail.com>
2017-02-14 19:08:50 -0800

    finished mastery

--- FoundationsMasteryTests/CustomStringTests.cs
+++ FoundationsMasteryTests/CustomStringTests.cs
@@ -10,14 +10,6 @@ namespace FoundationsMasteryTests
     public class CustomStringTests
     {
         // Instance of is not null ----- DONE
-        // Contents is not null when passing in ienumerable
-        // .Length gives accurate count of ienumerable
-        // .Clear removes all characters from .Content
-        // .Clear leaves you with a list/array/ienumerable (NOT NULL)
-        // concat makes expected string
-        // interleave makes expected string
-        // print makes correct string
-
         [TestMethod]
         public void EnsureICanCreateAnInstance()
         {
@@ -28,12 +20,15 @@ namespace FoundationsMasteryTests
             Assert.IsNotNull(myString);
         }
 
+        // Contents is not null when passing in ienumerable
         [TestMethod]
         public void EnsureContentsIsNotNull()
         {
             // Arrange
-            // Act
+            CustomString myString = new CustomString(new List<char>());
+            var testContents = myString.Contents;
             // Assert
+            Assert.IsNotNull(testContents);
         }
 
         [TestMethod]
@@ -45,6 +40,52 @@ namespace FoundationsMasteryTests
             Assert.AreEqual(myString.Length, mychars.Length);
             Assert.IsNotNull(myString.Contents);
             CollectionAssert.AreEqual(mychars.ToList(), myString.Contents.ToList());
+        }
+
+        // .Length gives accurate count of ienumerable
+        [TestMethod]
+        public void EnsureCanGrabLength()
+        {
+            CustomString myString = new CustomString(new List<char> { 'o', 'h', 'i'});
+            // Assert
+            Assert.AreEqual(3, myString.Length);
+        }
+
+        // .Clear removes all characters from .Content
+        // .Clear leaves you with a list/array/ienumerable (NOT NULL)
+        [TestMethod]
+        public void EnsureClearMakesEmpty()
+        {
+            CustomString myString = new CustomString(new List<char> { 'o', 'h', 'i', 'o' });
+            myString.Clear();
+            Assert.AreEqual(0, myString.Length);
+            Assert.IsNotNull(myString.Contents);
+        }
+
+        // concat makes expected string
+        [TestMethod]
+        public void EnsureConcatMakesString()
+        {
+            CustomString myString = new CustomString(new List<char> { 'o', 'h', 'i', 'o' });
+            CustomString anotherString = new CustomString(new List<char> { 'r', 'i', 'v', 'e', 'r' });
+            var answer = myString.Concat(anotherString.Contents);
+            Assert.AreEqual("ohioriver", answer);
+        }
+        // interleave makes expected string
+        public void EnsureInterleaveMakesZipper()
+        {
+            CustomString myString = new CustomString(new List<char> { 'o', 'h', 'i', 'o' });
+            CustomString anotherString = new CustomString(new List<char> { 'r', 'i', 'v', 'e', 'r' });
+            var answer = myString.Interleave(anotherString.Contents);
+            Assert.AreEqual("orhiivor", answer);
+        }
+
+        // print makes correct string
+        public void EnsureCanPrintString()
+        {
+            CustomString myString = new CustomString(new List<char> { 'o', 'h', 'i', 'o' });
+            var answer = myString.Print();
+            Assert.AreEqual("ohio", answer);
         }
 
     }
