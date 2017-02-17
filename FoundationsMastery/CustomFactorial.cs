f0d271158a06652f920fd2cac7f822094d2e9c62
Alex Saber <alexsaber89@gmail.com>
2017-02-17 15:54:55 -0600

    initialize CustomFactorial and tests

---
+++ FoundationsMastery/CustomFactorial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FoundationsMastery.Interfaces;
+
+namespace FoundationsMastery
+{
+    public class CustomFactorial : IFactorial
+    {
+        public CustomFactorial()
+        {
+        }
+
+        public int Iterative(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Recursive(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
