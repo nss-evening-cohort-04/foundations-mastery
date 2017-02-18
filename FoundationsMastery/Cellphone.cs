b5ef131c98d94451c1419d0b49b5fcb99753415b
sward42 <sarahward42@gmail.com>
2017-02-17 22:15:23 -0600

    add cellphone parent class

---
+++ FoundationsMastery/Cellphone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationsMastery
+{
+    public class Cellphone
+    {
+        public bool Touchscreen { get; set; }
+        public int MemoryGB { get; set; }
+        public string Model { get; set; }
+
+        public Cellphone()
+        {
+            this.MemoryGB = 0;
+        }
+
+        public Cellphone(int memory, bool screen, string model)
+        {
+            Touchscreen = screen;
+            MemoryGB = memory;
+            Model = model;
+        }
+
+        public virtual string OperatingSystem()
+        {
+            var OS = "Operating System used:";
+            return OS;
+        }
+
+        public virtual int MemoryRecall()
+        {
+            Console.WriteLine("Total Memory: 0GB");
+            return MemoryGB;
+        }
+    }
+}
