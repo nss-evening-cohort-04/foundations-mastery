ba9f634b27d949b6e689d89d5fd442bbf01a3f52
Alex Saber <Alex Saber>
2017-02-18 01:03:06 -0600

    implement CustomFactorial and tests

--- FoundationsMastery/CustomFactorial.cs
+++ FoundationsMastery/CustomFactorial.cs
@@ -9,14 +9,34 @@ namespace FoundationsMastery
         {
         }
 
-        public int Iterative(int n)
+        public int Iterative(int n) // good when n >= 1
         {
-            throw new NotImplementedException();
+            if (n < 1)
+            {
+                throw new ArgumentException("Iterative argument is less than 1.");
+            }
+            int iterativeOutput = 1;
+            for (int i=n; i >= 1; i--)
+            {
+                iterativeOutput = iterativeOutput * i;
+            }
+            return iterativeOutput;
         }
 
-        public int Recursive(int n)
+        public int Recursive(int n) // good when n >= 1
         {
-            throw new NotImplementedException();
+            if (n < 1)
+            {
+                throw new ArgumentException("Recursive argument is less than 1.");
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Recursive(n - 1);
+            }
         }
     }
 }
--- FoundationsMasteryTests/FactorialTests.cs
+++ FoundationsMasteryTests/FactorialTests.cs
@@ -18,25 +18,41 @@ namespace FoundationsMasteryTests
         [TestMethod]
         public void EnsureFactorialCalcInterative()
         {
+            CustomFactorial factorial = new CustomFactorial();
 
+            var expectedResult = 120;
+            var actualResult = factorial.Iterative(5);
+
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [TestMethod]
         public void EnsureFactorialCalcRecursive()
         {
+            CustomFactorial factorial = new CustomFactorial();
 
+            var expectedResult = 24;
+            var actualResult = factorial.Recursive(4);
+
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [TestMethod]
-        //Hint: what goes here to say an exception is expected?
+        [ExpectedException(typeof(ArgumentException))]
         public void EnsureFactorialCalcRecursiveFails()
         {
+            CustomFactorial factorial = new CustomFactorial();
+
+            var actualResult = factorial.Recursive(0);
         }
 
         [TestMethod]
-        //Hint: what goes here to say an exception is expected?
+        [ExpectedException(typeof(ArgumentException))]
         public void EnsureFactorialCalcIterativeFails()
         {
+            CustomFactorial factorial = new CustomFactorial();
+
+            var actualResult = factorial.Iterative(0);
         }
     }
 }
