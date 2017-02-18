6e3d1922b759ba3167f3074b937cd40af4ac72cf
sward42 <sarahward42@gmail.com>
2017-02-17 23:22:14 -0600

    create samsung child class

---
+++ FoundationsMastery/Samsung.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationsMastery
+{
+    public class Samsung : Cellphone
+    {
+        public Samsung(int memory, bool screen, string model)
+        {
+            Touchscreen = screen;
+            MemoryGB = memory;
+            Model = model;
+        }
+
+        public override string OperatingSystem()
+        {
+            var OS = "Operating System used: Android";
+            return OS;
+        }
+
+        public override int MemoryRecall()
+        {
+            Console.WriteLine($"Total Memory: {MemoryGB}GB");
+            return MemoryGB;
+        }
+    }
+}
---
+++ FoundationsMasteryTests/SamsungTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FoundationsMastery;
+
+namespace FoundationsMasteryTests
+{
+    [TestClass]
+    public class SamsungTests
+    {
+        [TestMethod]
+        public void EnsureICanCreateAnInstanceOfSamsung()
+        {
+            Samsung myCellphone = new Samsung(16, true, "Galaxy S7");
+            Assert.IsNotNull(myCellphone);
+        }
+
+        [TestMethod]
+        public void EnsureOperatingSystemReturnsProperString()
+        {
+            Samsung myCellphone = new Samsung(16, true, "Galaxy S7");
+
+            var expectedResult = "Operating System used: Android";
+            var actualResult = myCellphone.OperatingSystem();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void EnsureMemoryRecallReturnsProperInt()
+        {
+            Samsung myCellphone = new Samsung(16, true, "Galaxy S7");
+
+            var expectedResult = 16;
+            var actualResult = myCellphone.MemoryRecall();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
