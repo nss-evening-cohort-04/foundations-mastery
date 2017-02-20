50a6aa4417979947da9ac72d4ea3a0b593fb8cb7
Josh Kilgore <kilgore.josh@gmail.com>
2017-02-20 17:12:49 -0600

    add pets and dogs classes set up inheritence

--- FoundationsMasteryTests/PetsTest.cs
+++ FoundationsMasteryTests/PetsTest.cs
@@ -4,10 +4,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FoundationsMasteryTests
 {
     [TestClass]
-    public class UnitTest1
+    public class AnimalsTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void EnsureICanCreateInstanceAnimals()
         {
         }
     }
